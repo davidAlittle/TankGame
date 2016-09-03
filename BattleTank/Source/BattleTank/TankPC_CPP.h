81cf4db88b752813822610df5b9c158370f2456f
DavidALittle <David@davidallenlittle.com>
2016-09-03 18:09:55 -0500

    Checkin from VS

---
+++ BattleTank/Source/BattleTank/TankPC_CPP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "TankPC_CPP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPC_CPP : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ BattleTank/Source/BattleTank/TankPlayerContorller_BP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerContorller_BP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerContorller_BP : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
